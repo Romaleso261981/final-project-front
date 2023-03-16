cf2fbd34050adfab861be84657bedbf1f7b88201
Roman lesyo <ladiginscormag@gmail.com>
2023-03-16 15:07:23 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../Header/Header";
 
 export const SharedLayout = () => {
   return (
     <>
       <h1>SharedLayout</h1>
+      <Header  />
       <Outlet />
     </>
   );
