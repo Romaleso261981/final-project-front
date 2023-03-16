140b5adde7a0c40a68eccb8e14ca71a97ea612a8
Roman lesyo <ladiginscormag@gmail.com>
2023-03-16 10:54:37 +0200

    some fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
   useSearchParams,
   useNavigate,
 } from "react-router-dom";
-import { Loader } from "./components/Loader/Loader";
 
 import { authToken } from "./API";
 
@@ -20,22 +19,23 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 const LoginPage = lazy(() => import("./pages/LoginsPage/Logins"));
-const Register = lazy(() => import("./pages/RegisterPage/Register"));
-const Layout = lazy(() => import("./components/Layout/Layout"));
-const Wallet = lazy(() => import("./pages/WalletPage/Wallet"));
-const ReportsPage = lazy(() => import("./pages/ReportsPage/ReportsPage"));
+const NewsPage = lazy(() => import("./pages/LoginsPage/Logins"));
+const NoticesPage = lazy(() => import("./pages/LoginsPage/Logins"));
+const OurFriendsPage = lazy(() => import("./pages/LoginsPage/Logins"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage/Register"));
+const SharedLayout = lazy(() => import("./components/Layout/Layout"));
+const UserPage = lazy(() => import("./pages/WalletPage/Wallet"));
 
 const PrivateRoute = ({ children, token }) => {
   return token ? children : <Navigate to="/" />;
 };
 
 const PublicRoute = ({ children, token }) => {
-  return !token ? children : <Navigate to="/wallet" />;
+  return !token ? children : <Navigate to="/NewsPage" />;
 };
 
 export function App() {
   const dispatch = useDispatch();
-  const [isHintShown, setIsHintShown] = useState(false);
   const token = useSelector(selectAccessToken);
   // const token = null;
   const selectedMode = useSelector(getMode);
@@ -53,66 +53,22 @@ export function App() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const accessToken = searchParams.get("accessToken");
-    const refreshToken = searchParams.get("refreshToken");
-    const sid = searchParams.get("sid");
-    if (!accessToken) return;
-    dispatch(googleAuthUser({ accessToken, refreshToken, sid }));
-    navigate("/wallet");
-  }, [searchParams, dispatch, navigate]);
+  // useEffect(() => {
+  //   const accessToken = searchParams.get("accessToken");
+  //   const refreshToken = searchParams.get("refreshToken");
+  //   const sid = searchParams.get("sid");
+  //   if (!accessToken) return;
+  //   dispatch(googleAuthUser({ accessToken, refreshToken, sid }));
+  //   navigate("/wallet");
+  // }, [searchParams, dispatch, navigate]);
 
   return (
     <ThemeProvider theme={themeMode}>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Layout />
-            </Suspense>
-          }
-        >
-          <Route
-            index
-            element={
-              <Suspense fallback={<Loader />}>
-                <PublicRoute token={token}>
-                  <LoginPage />
-                </PublicRoute>
-              </Suspense>
-            }
-          />
-           <Route
-            path="/register"
-            element={
-              <PublicRoute token={token}>
-                <Register />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/wallet"
-            element={
-              <Suspense fallback={<Loader />}>
-                <PrivateRoute token={token}>
-                  <Wallet
-                    setIsHintShown={setIsHintShown}
-                    isHintShown={isHintShown}
-                  />
-                </PrivateRoute>
-              </Suspense>
-            }
-          />
-          <Route
-            path="/reports"
-            element={
-              <PrivateRoute token={token}>
-                <ReportsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<h1>Невірно прописаний шлях</h1>} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="news/" element={<NewsPage />}></Route>
+          <Route path="notices/" element={<NoticesPage />}></Route>
+          <Route path="friends/" element={<OurFriendsPage />}></Route>
         </Route>
       </Routes>
     </ThemeProvider>
