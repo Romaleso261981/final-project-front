fe4a4425367b28351ad83c2eea5a4551cb387231
Roman lesyo <ladiginscormag@gmail.com>
2023-03-16 15:06:24 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <>
       <h1>Header</h1>
+      <ul>
+        <li>
+          <Link to="auth">auth</Link>
+          <Link to="auth/register">register</Link>
+          <Link to="auth/login">login</Link>
+          <Link to="user">user</Link>
+          <Link to="friends">friends</Link>
+          <Link to="user/news"></Link>
+          <Link to="user/notices">notices</Link>
+          <Link to="user/notices/sell">sell</Link>
+          <Link to="user/notices/lost-found">lost-found</Link>
+          <Link to="user/notices/for-free">for-free</Link>
+        </li>
+      </ul>
     </>
   );
 };
